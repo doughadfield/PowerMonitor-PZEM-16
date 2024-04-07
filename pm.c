6cc27333b009aac70e28e61c9dc8f264423a027c
doughadfield <doug.hadfield@btinternet.com>
2024-04-07 13:10:27 +0100

    Added logging to separate named file, and forking separate log process Still to do: create new log file every period (24 hrs)

--- pm.c
+++ pm.c
@@ -11,7 +11,7 @@
  * use site https://crccalc.com/ to calculate CRC (MODBUS 16bit format)
  */
 
-#define INTERVAL 60
+#define INTERVAL 1              // logging interval in seconds
 #define TERMINAL    "/dev/ttyUSB0"
 
 #include <errno.h>
@@ -22,13 +22,20 @@
 #include <termios.h>
 #include <unistd.h>
 #include <time.h>
-#include <stdbool.h>
+#include <signal.h>
 
 /*
  * Global variables
  */
 char *progname;					// holds our program name (argv[0]) globally
-bool logging = false;			// flag to indicate whether we're logging mode
+
+float voltage;                  // values read from device
+float current;
+float power;
+float energy;
+float frequency;
+float factor;
+unsigned int alarmset;
 
 int set_interface_attribs(int fd, int speed)
 {
@@ -57,9 +64,11 @@ int set_interface_attribs(int fd, int speed)
 	tty.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
 	tty.c_oflag &= ~OPOST;
 
-	/* fetch bytes as they become available */
-	tty.c_cc[VMIN] = 1;
-	tty.c_cc[VTIME] = 1;
+	/* timed read - wait for VTIME (tenths of) secs then return
+     * regardless of number of chars read
+     */
+	tty.c_cc[VMIN] = 0;         // don't wait for min chars read
+	tty.c_cc[VTIME] = 5;        // timeout and return after half a sec if no chars read
 
 	if (tcsetattr(fd, TCSANOW, &tty) != 0)
 	{
@@ -67,28 +76,6 @@ int set_interface_attribs(int fd, int speed)
 		return -1;
 	}
 	return 0;
-}
-
-/*
- * Set VMIN and VTIME on the serial port
- * to control read timeout etc.
- * explanation at http://unixwiz.net/techtips/termios-vmin-vtime.html
- */
-void set_mincount(int fd, int mcount)
-{
-	struct termios tty;
-
-	if (tcgetattr(fd, &tty) < 0)
-	{
-		fprintf(stderr, "Error tcgetattr: %s\n", strerror(errno));
-		return;
-	}
-
-	tty.c_cc[VMIN] = mcount ? 1 : 0;												// 0 = timed read - timeout if no chars received
-	tty.c_cc[VTIME] = 5;															// number of tenths of seconds to wait
-
-	if (tcsetattr(fd, TCSANOW, &tty) < 0)
-		fprintf(stderr, "Error tcsetattr: %s\n", strerror(errno));
 }
 
 /*
@@ -108,7 +95,6 @@ int openserial(char *portname)
 
 	/*baudrate B9600, 8 bits, no parity, 1 stop bit */
 	set_interface_attribs(fd, B9600);
-	set_mincount(fd, 0);															/* set to pure timed read */
 	return fd;
 }
 
@@ -118,52 +104,28 @@ int openserial(char *portname)
  * This function should really put the result values into a structure,
  * but I'm lazy and just used separate variables
  */
-int displayvalues(int fd)
+int readvalues(int fd)
 {
 	unsigned char buf[22];
 	int rdlen;
-	unsigned int voltage;
-	unsigned int current;
-	unsigned int power;
-	unsigned int energy;
-	unsigned int frequency;
-	unsigned int factor;
-	unsigned int alarm;
 
-	rdlen = read(fd, buf, 22);
-
-#ifdef DEBUG
-	fprintf(stderr, "read %d characters from serial port\n", rdlen);
-#endif							// DEBUG
+	rdlen = read(fd, buf, 22);          // read remaining message from device
 
 	if (rdlen < 22)
 	{
 		fprintf(stderr, "incorrect message length received: %d.\n", rdlen);
 		return -1;
 	}
-	/* print values received from power monitor */
-	voltage = (buf[0] << 8) | buf[1];
-	current = (buf[4] << 24) | (buf[5] << 16) | (buf[2] << 8) | buf[3];
-	power = (buf[8] << 24) | (buf[9] << 16) | (buf[6] << 8) | buf[7];
-	energy = (buf[12] << 24) | (buf[13] << 16) | (buf[10] << 8) | buf[11];
-	frequency = (buf[14] << 8) | buf[15];
-	factor = (buf[16] << 8) | buf[17];
-	alarm = (buf[18] << 8) | buf[19];
-
-	if (logging)
-	{
-		printf("%.1f,%.2f,%.2f,%.2f,%.1f,%.2f\n", 
-			   (float) voltage / 10, (float) current / 1000,
-			   (float) power / 10000, (float) energy / 1000,
-			   (float) frequency / 10, (float) factor / 100);
-	} else
-	{
-		printf
-			("voltage = %f\ncurrent = %f\npower = %f\nenergy = %f\nFrequency = %f\nfactor = %f\nalarm = %d\n",
-			 (float) voltage / 10, (float) current / 1000,
-			 (float) power / 10000, (float) energy / 1000,
-			 (float) frequency / 10, (float) factor / 100, alarm);
-	}
+	/* calculate parameter values from returned chars
+     * and store in global variables
+     */
+	voltage = (float)((buf[0] << 8) | buf[1]) / 10;
+	current = (float)((buf[4] << 24) | (buf[5] << 16) | (buf[2] << 8) | buf[3]) /1000;
+	power = (float)((buf[8] << 24) | (buf[9] << 16) | (buf[6] << 8) | buf[7]) /10000;
+	energy = (float)((buf[12] << 24) | (buf[13] << 16) | (buf[10] << 8) | buf[11]) /1000;
+	frequency = (float)((buf[14] << 8) | buf[15]) /10;
+	factor = (float)((buf[16] << 8) | buf[17]) /100;
+	alarmset = (buf[18] << 8) | buf[19];
 	return 0;
 }
 
@@ -240,33 +202,38 @@ int setaddress(int fd, int newaddress)
 }
 
 /*
- * take reading from power monitor
+ * Send command and receive first part of response from power monitor
  * fd is opened serial port
- * buf is address of result buffer (needs to be 26 chars min)
- */
-int takereading(int fd)
-{
-/*
- * command string to sent to device. NOTE last two digits are CRC in REVERSE ORDER
+ * Master (this machine) sends a command string to the power unit
+ * and if it's correctly formatted (with correct CRC) the unit
+ * responds with about 26 characters of message. 
+ * This message is read into the serial port buffer automatically,
+ * so when we read from the port, we're usually just reading from the
+ * linux port buffer, which returns immediately.
+ * This function reads the first 3 chars of the returned message,
+ * to determine the correct response. The rest of the message stays in
+ * the port buffer, to be read by the next function.
+ *
+ * NOTE last two digits of command string (xstr) are CRC in REVERSE ORDER
  * use site https://crccalc.com/ to calculate CRC (MODBUS 16bit format)
+ * first char of message is device address. 0xF8 is "universal" address 
+ * (all devices on bus will respond, so only use if a single device on bus)
+ * Most purchased PZEM-16 devices are shipped with address 0x01
  */
+int sendcommand(int fd)
+{
 	// unsigned char xstr[]={0xF8, 0x04, 0x00, 0x00, 0x00, 0x0A, 0x64, 0x64};
-	unsigned char xstr[] =
-		{ 0x01, 0x04, 0x00, 0x00, 0x00, 0x0A, 0x70, 0x0D };
-	int xlen = 8;				// length of above command string
+	unsigned char xstr[] = {0x01, 0x04, 0x00, 0x00, 0x00, 0x0A, 0x70, 0x0D};
+    int xlen = 8;               // length of command string to send (inc CRC)
 	int wrlen;					// number of chars actually written to serial port
 	int count;					// generic counter for loops
 	int rdlen;					// number of chars actually read from serial port
 	unsigned char buf[4];		// temp storage for returned chars read from port
 
 	/* send command to power unit */
-	wrlen = write(fd, xstr, xlen);
+	wrlen = write(fd, xstr, xlen); // write command string to device 
 
-#ifdef DEBUG
-	printf("Written %d chars\n", wrlen);											//DEBUG
-#endif							// DEBUG
-
-	if (wrlen != xlen)
+	if (wrlen != xlen)          // Not all chars were written, so write error
 	{
 		fprintf(stderr, "Error from write: %d, %d\n", wrlen, errno);
 		return -1;
@@ -305,19 +272,44 @@ int takereading(int fd)
  * Create initial logfile on startup, then new logfiles every period (24 hrs?)
  * name logfiles with timestamp
  */
+#define TIMEFILESTRING "pm-%Y-%m-%dT%H%M"           // time string in format suitable for filename (ISO 8601)
+#define TIMELOGSTRING "%Y:%b:%d:%a:%X"              // time string in format suitable for log entry
+
 void logloop(int fd)
 {
-	logging = true;																	// set global logging flag, to print in log format
     char timestamp[26];                                                             // buffer to hold timestamp string
+    FILE *logfile;                                                                  // file pointer for log file
+    pid_t pid;                                      // pid of process (parent or child)
+
+    pid = fork();                                  // fork a new process to run in background
+    if(pid < 0)                                     // fork didn't work if returns less than 0
+        perror("Fork failed!");                     // print error and exit program
+
+    if(pid)                                         // pid > zero, so this is the parent
+    {
+        printf("Logging process running in background. PID = %d\n\n", pid);       // in parent, fork returns PID of child
+        exit(0);                                                                // child does the work now
+    }
+    /* if we get here, we should be child process */
+
+    if( signal(SIGHUP, SIG_IGN) == SIG_ERR)                                                        // ignore hangup so calling terminal can exit
+        perror("Failed to ignore SIGHUP signal");                                   // print error and exit if we can't ignore SIGHUP
+
+    time_t clk = time(NULL);                                                   	// get current time
+    strftime(timestamp, 26, TIMEFILESTRING, localtime(&clk));                 // format timestamp suitable for filename and log entry
+    logfile = fopen(timestamp,"w");                                                 // create initial logfile when prog starts
+    if(logfile == NULL)                                                             // create initial logfile when prog starts
+        perror("cannot open logfile");                                              // cannot open log file for some reason
 
 	while (1)																		// loop forever taking readings
 	{
 		time_t clk = time(NULL);                                                   	// get current time
-        strftime(timestamp, 26, "%Y:%b:%d:%a:%X", localtime(&clk));                 // format timestamp suitable for filename and log entry
-        printf("%s", timestamp);                                                    // output timestamp as first part of log line
-		takereading(fd);															// send command and recieve initial ack from device
-		displayvalues(fd);                                                          // read rest of data from device and present
-		fflush(stdout);																// flush output to logfile
+        strftime(timestamp, 26, TIMELOGSTRING, localtime(&clk));                 // format timestamp suitable for filename and log entry
+		sendcommand(fd);															// send command and recieve initial ack from device
+		readvalues(fd);                                                             // read rest of data from device and present
+		fprintf(logfile, "%s,%.1f,%.2f,%.2f,%.2f,%.1f,%.2f\n",
+                timestamp, voltage, current, power, energy, frequency, factor);
+		fflush(logfile);    														// flush output to logfile
 		sleep(INTERVAL);															// repeat every INTERVAL seconds
 	}
 }
@@ -356,8 +348,11 @@ int main(int argc, char *argv[])
 	switch (argc)																	// test for arguments
 	{
 	case 1:																	    	// no args, so just take a human readable reading and exit
-		takereading(fd);															// send command and recieve initial ack from device
-		displayvalues(fd);                                                          // read rest of data from device and present
+		sendcommand(fd);															// send command and recieve initial ack from device
+		readvalues(fd);                                                             // read rest of data from device 
+		printf                                                                      // print values to stdout
+			("voltage = %f\ncurrent = %f\npower = %f\nenergy = %f\nFrequency = %f\nfactor = %f\nalarm = %x\n",
+			 voltage, current, power, energy, frequency, factor, alarmset);
 		exit(0);
 
 	case 2:																	    	// only one argument, so should be "logging"
